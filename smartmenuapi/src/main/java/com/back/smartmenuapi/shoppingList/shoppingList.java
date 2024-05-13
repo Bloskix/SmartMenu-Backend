63e35c217094c7bd01d93314e805407e89b40422
pablo <p.coello@alumno.um.edu.ar>
2024-05-13 17:27:16 -0300

    list of ingredients function created

--- smartmenuapi/src/main/java/com/back/smartmenuapi/shoppingList/shoppingList.java
+++ smartmenuapi/src/main/java/com/back/smartmenuapi/shoppingList/shoppingList.java
@@ -1,12 +1,16 @@
 package com.back.smartmenuapi.shoppingList;
 
+import com.back.smartmenuapi.day.Day;
 import com.back.smartmenuapi.ingredient.Ingredient;
+import com.back.smartmenuapi.meal.Meal;
+import com.back.smartmenuapi.week.Week;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Entity
@@ -23,4 +27,17 @@ public class shoppingList {
     @Column(name = "ingredients_to_buy")
     @OneToMany(cascade = CascadeType.ALL)
     private List<Ingredient> ingredientsToBuy;
+
+    public List<Ingredient> getIngredientsToBuy(Week week) {
+        List<Ingredient> ingredientsToBuy = new ArrayList<>();
+        for (Day day : week.getDays()) {
+            Meal meal = day.getMeal();
+            ingredientsToBuy.addAll(meal.getIngredients());
+            //Cuando haya mas de una comida por dia este codigo se descomenta
+//            for (Meal meal : day.getMeal()) {
+//                ingredientsToBuy.addAll(meal.getIngredients());
+//            }
+        } return ingredientsToBuy;
+    }
 }
+
